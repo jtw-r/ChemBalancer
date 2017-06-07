8b1562b217b6c7c7748e22d76453f33cbcf94994
thejonathanr <jonathan.reiterman@gmail.com>
2017-06-06 20:21:54 -0700

    Updated

--- ChemBalancer/Program.cs
+++ ChemBalancer/Program.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace ChemBalancer {
 	class Program {
+
+		[STAThread]
 		static void Main(string[] args) {
+			Program pr = new Program();
+			pr.CommandLineHandler();
+		}
+
+		public void CommandLineHandler() {
+
 		}
 	}
 }
